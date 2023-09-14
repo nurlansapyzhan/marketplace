7f0c4634494c4e2822841c483344567191401f8b
Alibek Beket <alibek1beket@gmail.com>
2023-09-14 15:11:17 +0600

    #9 перенес dataabase.py на уровень выше

--- backend/src/main.py
+++ backend/src/main.py
@@ -3,7 +3,7 @@ from fastapi_users import FastAPIUsers
 from fastapi import FastAPI, Depends
 
 from src.auth.auth import auth_backend
-from src.auth.database import User
+from src.database import User
 from src.auth.manager import get_user_manager
 from src.auth.schemas import UserRead, UserCreate
 
